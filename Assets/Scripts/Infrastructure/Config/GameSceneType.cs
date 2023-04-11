4ce4add2c400461348c31f44182ecaae12edc358
Molodcov Igor <molodcov.i321@gmail.com>
2023-04-11 20:54:43 +0500

    Создал новую сцену

--- Assets/Scripts/Infrastructure/Config/GameSceneType.cs
+++ Assets/Scripts/Infrastructure/Config/GameSceneType.cs
@@ -2,6 +2,7 @@
 {
     public enum GameSceneType
     {
-        GameScene1
+        GameScene1,
+        GameScene2,
     }
 }
--- Assets/Scripts/Infrastructure/States/MainGameState.cs
+++ Assets/Scripts/Infrastructure/States/MainGameState.cs
@@ -4,11 +4,13 @@ namespace Infrastructure
 {
     public class MainGameState : IGameState
     {
+        private readonly BootstrapConfig _bootstrapConfig;
         private readonly ScenesConfig _scenesConfig;
         private readonly LoadingScene _loadingScene;
 
-        public MainGameState(ScenesConfig scenesConfig, LoadingScene loadingScene)
+        public MainGameState(BootstrapConfig bootstrapConfig, ScenesConfig scenesConfig, LoadingScene loadingScene)
         {
+            _bootstrapConfig = bootstrapConfig;
             _scenesConfig = scenesConfig;
             _loadingScene = loadingScene;
         }
@@ -16,7 +18,7 @@ namespace Infrastructure
         public void Enter()
         {
             Time.timeScale = 1;
-            _scenesConfig.TryGetGameScene(GameSceneType.GameScene1, out string sceneName);
+            _scenesConfig.TryGetGameScene(_bootstrapConfig.StartScene, out string sceneName);
             _loadingScene.LoadScene(sceneName);
         }
     }
